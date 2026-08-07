ac3735c6f01bcb3c79a4518dd26dafb93fcbca8c
kevinseguin-adm <kevin.seguin@cccc.org>
2026-08-07 15:49:39 -0400

    Add HR process area prototype

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,5 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://kevinseguin-adm.github.io',
   base: '/cccc-staff-site-poc',
+  trailingSlash: 'always',
 });
